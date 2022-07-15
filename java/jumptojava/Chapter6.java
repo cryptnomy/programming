5da36819d7d1c362b49650bc7b9fb135621f5b86
gtbprog <gtbprog@gmail.com>
2022-07-16 01:02:50 +0900

    2022-07-16 Sat. 01:02 java update

--- java/jumptojava/Chapter6.java
+++ java/jumptojava/Chapter6.java
@@ -1,8 +1,55 @@
 package jumptojava;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
 
 public class Chapter6 {
-    
+    public static void main(String[] args) throws IOException {
+        // Q1. Sum of two integers
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int res = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+        System.out.println(res);
+        // Q2. Convert to capital letters until user types "END"
+        while (true) {
+            BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Type in English sentece: ");
+            String line = br2.readLine();
+            if ("END".equals(line)) break;
+            System.out.println(line.toUpperCase());
+        }
+        // Q3. Save a file and then read it
+        FileWriter fw3 = new FileWriter("WriteOnce.txt");
+        fw3.write("Write once, run anywhere\n");
+        fw3.close();
+        BufferedReader br3 = new BufferedReader(new FileReader("WriteOnce.txt"));
+        String line3 = br3.readLine();
+        System.out.println(line3);
+        // Q4. Write a user input to a file
+        FileWriter fw4 = new FileWriter("KeepContents.txt", true);
+        System.out.println("Enter a sentence: ");
+        BufferedReader br4 = new BufferedReader(new InputStreamReader(System.in));
+        String line4 = br4.readLine();
+        fw4.write(String.format("%s\n", line4));
+        fw4.close();
+        // Q5. Replace contents in a file
+        ArrayList<String> al5 = new ArrayList<>();
+        BufferedReader br5 = new BufferedReader(new FileReader("PythonSaying.txt"));
+        while (true) {
+            String line5 = br5.readLine();
+            if (line5 == null) break;
+            al5.add(line5);
+        }
+        br5.close();
+        String text5 = String.join("\n", al5);
+        text5 = text5.replaceAll("Python", "Java");
+        FileWriter fw5 = new FileWriter("PythonSaying.txt");
+        fw5.write(text5);
+        fw5.close();
+    }
 }
--- java/jumptojava/SampleFileIO.java
+++ java/jumptojava/SampleFileIO.java
@@ -9,36 +9,38 @@ import java.io.PrintWriter;
 
 public class SampleFileIO {
     public static void main(String[] args) throws IOException {
+        // String filepath = "c:/Users/after/Documents/Programming/java/jumptojava/";
+        String filepath = "d:/GT/Programming/java/jumptojava/";
         // FileOutputStream ... string -> byte conversion needed
-        FileOutputStream fos = new FileOutputStream("c:/Users/after/Documents/Programming/java/jumptojava/out_FileOutputStream.txt");
+        FileOutputStream fos = new FileOutputStream(filepath + "out_FileOutputStream.txt");
         for (int i = 1; i < 11; i++) {
             String data = i + "th line\r\n";
             fos.write(data.getBytes()); 
         }
         fos.close();
         // FileWriter
-        FileWriter fw = new FileWriter("c:/Users/after/Documents/Programming/java/jumptojava/out_FileWriter.txt");
+        FileWriter fw = new FileWriter(filepath + "out_FileWriter.txt");
         for (int i = 1; i < 11; i++) {
             String data = i + "th line\r\n";
             fw.write(data);
         }
         fw.close();
         // PrintWriter
-        PrintWriter pw = new PrintWriter("c:/Users/after/Documents/Programming/java/jumptojava/out_PrintWriter.txt");
+        PrintWriter pw = new PrintWriter(filepath + "out_PrintWriter.txt");
         for (int i = 1; i < 11; i++) {
             String data = i + "th line";
             pw.println(data);
         }
         pw.close();
         // Add contents to a file
-        FileWriter fw2 = new FileWriter("c:/Users/after/Documents/Programming/java/jumptojava/out_FileWriter.txt", true);
+        FileWriter fw2 = new FileWriter(filepath + "out_FileWriter.txt", true);
         for (int i = 11; i < 21; i++) {
             String data = i + "th line.\r\n";
             fw2.write(data);
         }
         fw2.close();
         // BufferedReader, FileReader
-        BufferedReader br = new BufferedReader(new FileReader("c:/Users/after/Documents/Programming/java/jumptojava/out_FileWriter.txt"));
+        BufferedReader br = new BufferedReader(new FileReader(filepath + "out_FileWriter.txt"));
         while (true) {
             String line = br.readLine();
             if (line == null) break;
