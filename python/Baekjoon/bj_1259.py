0a5f6c89b6c6f928c49c478efa0ec5af24735407
gtbprog <gtbprog@gmail.com>
2022-08-05 12:08:23 +0900

    2022-08-05 Fri. 12:08 minor update

--- python/Baekjoon/bj_1259.py
+++ python/Baekjoon/bj_1259.py
@@ -1,8 +1,34 @@
+# while True:
+#     n = input()
+#     if n == '0':
+#         break
+#     if n == n[::-1]:
+#         print('yes')
+#     else:
+#         print('no')
+
+# while True:
+#     n = int(input())
+#     if (n == 0):
+#         break
+#     x, y = n, 0
+#     while x >= y:
+#         y = 10 * y + x % 10
+#         x //= x
+#     if x == y or x // 10 == y:
+#         print('yes')
+#     else:
+#         print('no')
+
 while True:
-    n = input()
-    if n == '0':
+    x = int(input())
+    if (x == 0):
         break
-    if n == n[::-1]:
-        print('yes')
-    else:
+    if x < 0 or (x % 10 == 0 and x != 0):
         print('no')
+        continue
+    y = 0
+    while x > y:
+        y = 10 * y + x % 10
+        x //= 10
+    print('yes' if (x == y or x == y//10) else 'no')
